74b1b9be72164e3e4bcc4444a8eac552ad443723
Scott Shealy <vsshealy@gmail.com>
2023-08-25 16:31:22 -0400

    Added PHP files

---
+++ php/menus/menu-members-primary.php
@@ -0,0 +1,18 @@
+<?php 
+    /**
+     * php/menus/menu-members-primary.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0 (2023.08.25)
+     * @copyright 2023 (2023.08.25)
+    **/
+?>
+
+<?php 
+    wp_nav_menu(
+        array(
+            'theme_location' => 'menu-9',
+            'menu_id' => 'Members-Primary'
+        )
+    );
+?>
